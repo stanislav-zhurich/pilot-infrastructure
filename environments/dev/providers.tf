b40f33bbc75c99e3948e19c426e33b6fd59b4152
Stanislav Zhurich <stanislav_zhurich@epam.com>
2023-05-15 14:33:40 +0300

    Add azuread provider

--- environments/dev/providers.tf
+++ environments/dev/providers.tf
@@ -7,3 +7,13 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
   subscription_id = var.subscription_id
 }
+
+provider "azuread" {
+  features {
+
+  }
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+}

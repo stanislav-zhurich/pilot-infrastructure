5ff0859584a913204a7533a7749e4db24400ad46
Stanislav Zhurich <stanislav_zhurich@epam.com>
2023-06-10 22:48:09 +0300

    add custom role

--- modules/cosmosdb/main.tf
+++ modules/cosmosdb/main.tf
@@ -19,3 +19,18 @@ resource "azurerm_cosmosdb_account" "cosmosdb_account" {
     failover_priority = 0
   }
 }
+
+resource "azurerm_cosmosdb_sql_role_definition" "cosmosdb_data_contributor_role" {
+  name                = "DataContributorRole"
+  resource_group_name = var.resource_group_name
+  account_name        = azurerm_cosmosdb_account.cosmosdb_account.name
+  type                = "CustomRole"
+  assignable_scopes   = [azurerm_cosmosdb_account.cosmosdb_account.id]
+
+  permissions {
+    data_actions = ["Microsoft.DocumentDB/databaseAccounts/readMetadata",
+        "Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/items/read", 
+        "Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/executeQuery",
+        "Microsoft.DocumentDB/databaseAccounts/sqlDatabases/containers/readChangeFeed"]
+  }
+}

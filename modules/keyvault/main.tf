11f023e2db46dd1077fd85977d6880207cf7da89
Stanislav Zhurich <stanislav_zhurich@epam.com>
2023-06-29 23:12:06 +0300

    add ado integration with keyvault

--- modules/keyvault/main.tf
+++ modules/keyvault/main.tf
@@ -1,3 +1,7 @@
+data "azuread_application" "ado_application" {
+  display_name = "dev_terraform_appreg"
+}
+
 resource "azurerm_key_vault" "key_vault" {
   name                        = var.key_vault_name
   location                    = var.location_name
@@ -12,7 +16,13 @@ resource "azurerm_key_vault" "key_vault" {
   tags = var.tags
 }
 
-resource "azurerm_role_assignment" "key_vault_contributor_assignement" {
+resource "azurerm_role_assignment" "ado_key_vault_contributor_assignement" {
+  scope                = azurerm_key_vault.key_vault.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azuread_application.ado_application.application_id
+}
+
+resource "azurerm_role_assignment" "terraform_key_vault_contributor_assignement" {
   scope                = azurerm_key_vault.key_vault.id
   role_definition_name = "Key Vault Administrator"
   principal_id         = var.current_user_id
